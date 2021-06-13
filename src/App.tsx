4e9160c2626d35dec97a2f02c9b77980b1fe7cb7
Blair Wilcox <reccanti@gmail.com>
2021-06-13 00:00:38 -0400

    Get images and stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,31 @@
-import { CircularProgress } from "@material-ui/core";
+import { CircularProgress, GridList, GridListTile } from "@material-ui/core";
 import "./App.css";
 import { useSetupContext, SetupProvider } from "./SetupManager";
 
-// function JohtoList() {
-//   const pokes: Array<{ name: string; index: number }> = [];
-
-//   pokemon.forEach((poke) => {
-//     const crystalDexEntry = poke.pokedex_numbers.find(
-//       (dex) => dex.pokedex.name === "original-johto"
-//     );
-//     if (crystalDexEntry) {
-//       pokes.push({
-//         name: poke.name,
-//         index: crystalDexEntry.entry_number,
-//       });
-//     }
-//   });
-
-//   pokes.sort((a, b) => {
-//     if (a.index > b.index) {
-//       return 1;
-//     } else if (a.index < b.index) {
-//       return -1;
-//     } else {
-//       return 0;
-//     }
-//   });
-
-//   return (
-//     <ol>
-//       {pokes.map((poke) => (
-//         <li key={poke.name}>{poke.name}</li>
-//       ))}
-//     </ol>
-//   );
-// }
-
-/**
- * Here's the thing Imma use to filter out all those sweet sweet pokeeeez
- */
-// function GenerationSelect() {
-//   const [gens, setGens] = useState<string[]>([]);
-//   const [currentGen, setCurrentGen] = useState<string>("");
-
-//   useEffect(() => {
-//     async function getGenerations() {
-//       const genList = await PokeAPI.Generaition.listAll();
-//       const genPromises = genList.results.map((gen) =>
-//         PokeAPI.Generaition.resolve(gen.name)
-//       );
-//       const gens = await Promise.all(genPromises);
-//       const genNames = gens.map((gen) => gen.name);
-//       setGens(genNames);
-//     }
-//     getGenerations().then();
-//   }, []);
-
-//   const handleChange = (event: ChangeEvent<{ value: unknown }>) => {
-//     setCurrentGen(event.target.value as string);
-//   };
-
-//   return (
-//     <FormControl>
-//       <InputLabel>Generation</InputLabel>
-//       <Select value={currentGen} onChange={handleChange}>
-//         <MenuItem value="">
-//           <em>Select a Generation</em>
-//         </MenuItem>
-//         {gens.map((gen) => (
-//           <MenuItem key={gen} value={gen}>
-//             {gen}
-//           </MenuItem>
-//         ))}
-//       </Select>
-//     </FormControl>
-//   );
-// }
-
-// function GenerationSelect() {
-//   const { generationNames } = useBoardConfigContext();
-// }
-
 function Loader() {
-  const { setupState } = useSetupContext();
-  if (setupState === "loading") {
-    return <CircularProgress />;
+  const { setupState, data } = useSetupContext();
+  if (
+    setupState === "ready" &&
+    data &&
+    "pokedex" in data &&
+    "generation" in data &&
+    "pokemon" in data
+  ) {
+    return (
+      <>
+        <pre>{data.generation.name}</pre>
+        <pre>{data.pokedex.name}</pre>
+        <GridList cellHeight={160} cols={15}>
+          {data.pokemon.map((pokee) => (
+            <GridListTile key={pokee.sprites.front_default} cols={1}>
+              <img src={pokee.sprites.front_default} alt={pokee.name} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </>
+    );
   } else {
-    return <div>H</div>;
+    return <CircularProgress />;
   }
 }
 
--- src/SetupManager.tsx
+++ src/SetupManager.tsx
@@ -1,11 +1,18 @@
 import { useState, ReactNode, useEffect } from "react";
-import PokeAPI, { IPokedex } from "pokeapi-typescript";
+import PokeAPI, { IPokedex, IGeneration, IPokemon } from "pokeapi-typescript";
 import { createCtx } from "./utils/createCtx";
 
 type SetupState = "loading" | "ready";
 
+interface State {
+  pokedex: IPokedex;
+  generation: IGeneration;
+  pokemon: IPokemon[];
+}
+
 interface SetupContext {
   setupState: SetupState;
+  data: State | null;
 }
 
 const [useCtx, Provider] = createCtx<SetupContext>();
@@ -16,24 +23,84 @@ interface Props {
   children: ReactNode;
 }
 
+/**
+ * Let's talk about the Board State! The Board State describes
+ * the configuration of the "board" that battleship will be
+ * played on (i.e. the arrangment of pokemon in a grid). Once the
+ * board is setup, it gets passed along to be used in the Game State
+ * (i.e. Where the ships are, what have been hit, and what's been guessed)
+ */
+
+// interface LoadingState {
+//   state: "loading";
+// }
+
+// interface ReadyState {
+//   state: "ready";
+//   data: {
+//     pokedex: IPokedex;
+//     generation: IGeneration;
+//   };
+// }
+
+// type BoardState = LoadingState | ReadyState;
+
 export function SetupProvider({ children }: Props) {
   const [setupState, setSetupState] = useState<SetupState>("loading");
+  const [pokedex, setPokedex] = useState<IPokedex | null>(null);
+  const [generation, setGeneration] = useState<IGeneration | null>(null);
+  const [pokemon, setPokemon] = useState<IPokemon[]>([]);
 
   useEffect(() => {
     const fetchAllData = async () => {
+      // fetch all the meta information
       const genPromise = PokeAPI.Generaition.resolve("generation-i");
       const dexPromise = PokeAPI.Pokedex.resolve("national");
       const [generation, pokedex] = await Promise.all([genPromise, dexPromise]);
-      console.log(generation);
-      console.log(pokedex);
+      setPokedex(pokedex);
+      setGeneration(generation);
+
+      // fetch the pokemon and sort it according to the pokedex
+      const pokeOrderMap = new Map<string, number>();
+      pokedex.pokemon_entries.forEach((entry) => {
+        pokeOrderMap.set(entry.pokemon_species.name, entry.entry_number);
+      });
+
+      const pokePromises = generation.pokemon_species.map((poke) =>
+        PokeAPI.Pokemon.resolve(poke.name)
+      );
+      const pokees = await Promise.all(pokePromises);
+      pokees.sort((a, b) => {
+        const aIndex = pokeOrderMap.get(a.name);
+        const bIndex = pokeOrderMap.get(b.name);
+        if (aIndex && bIndex) {
+          if (aIndex > bIndex) {
+            return 1;
+          } else if (aIndex < bIndex) {
+            return -1;
+          }
+        }
+        return 0;
+      });
+
+      setPokemon(pokees);
       setSetupState("ready");
     };
     fetchAllData();
   }, []);
 
-  const value = {
+  const value: SetupContext = {
     setupState,
+    data: null,
   };
+
+  if (pokedex && generation && pokemon) {
+    value.data = {
+      pokedex,
+      generation,
+      pokemon,
+    };
+  }
 
   return <Provider value={value}>{children}</Provider>;
 }
